11cf532884121afd436c53f3376388fe3949916a
joannakot <joanna.kot.ok@gmail.com>
2021-08-31 12:54:19 +0200

    javascript training basics

---
+++ 02-counter/app.js
@@ -0,0 +1,8 @@
+let count = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+	btn.addEventListener("click", function (e) {});
+});
