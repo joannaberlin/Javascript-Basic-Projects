816f49c19d77c305810233e77fc3e94ef6e4ed5f
joannakot <joanna.kot.ok@gmail.com>
2021-10-01 15:02:27 +0200

    cart updated, add to cart, total value

--- 23-comfy-store/src/cart/setupCart.js
+++ 23-comfy-store/src/cart/setupCart.js
@@ -10,7 +10,43 @@ import { findProduct } from "../store.js";
 import addToCartDOM from "./addToCartDOM.js";
 // set items
 
+const cartItemCountDOM = getElement(".cart-item-count");
+const cartItemsDOM = getElement(".cart-items");
+const cartTotalDOM = getElement(".cart-total");
+
+let cart = getStorageItem("cart");
+
 export const addToCart = (id) => {
-	console.log(id);
+	let item = cart.find((cartItem) => cartItem.id === id);
+	if (!item) {
+		let product = findProduct(id);
+		product = { ...product, amount: 1 };
+		cart = [...cart, product];
+		addToCartDOM(product);
+	} else {
+	}
+	displayCartItemCount();
+
+	displayCartTotal();
+
+	setStorageItem("cart", cart);
 	openCart();
 };
+
+function displayCartItemCount() {
+	const amount = cart.reduce((total, cartItem) => {
+		return (total += cartItem.amount);
+	}, 0);
+	cartItemCountDOM.textContent = amount;
+}
+function displayCartTotal() {
+	let total = cart.reduce((total, cartItem) => {
+		return (total += cartItem.price * cartItem.amount);
+	}, 0);
+	cartTotalDOM.textContent = `Total : ${formatPrice(total)}`;
+}
+
+const init = () => {
+	console.log(cart);
+};
+init();
